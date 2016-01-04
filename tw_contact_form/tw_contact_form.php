6138306eee2d40943fdd6cec2747bb131fe097db
tonmoysohaib <tonmoysohaib@gmail.com>
2016-01-04 14:02:30 +0600

    my new plugin and widget for contact

---
+++ tw_contact_form/tw_contact_form.php
@@ -0,0 +1,102 @@
+<?php
+/*
+Plugin Name: Example Contact Form Plugin
+Plugin URI: http://example.com
+Description: Simple non-bloated WordPress Contact Form
+Version: 1.0
+Author: tonmoy sohaib
+Author URI: http://w3guy.com
+*/
+
+function html_form_code() {
+	echo '<form action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
+	echo '<p>';
+	echo 'Your Name (required) <br/>';
+	echo '<input type="text" name="cf-name" pattern="[a-zA-Z0-9 ]+" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Email (required) <br/>';
+	echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Subject (required) <br/>';
+	echo '<input type="text" name="cf-subject" pattern="[a-zA-Z ]+" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" />';
+	echo '</p>';
+	echo '<p>';
+	echo 'Your Message (required) <br/>';
+	echo '<textarea rows="10" cols="35" name="cf-message">' . ( isset( $_POST["cf-message"] ) ? esc_attr( $_POST["cf-message"] ) : '' ) . '</textarea>';
+	echo '</p>';
+	echo '<p><input type="submit" name="cf-submitted" value="Send"></p>';
+	echo '</form>';
+}
+
+function deliver_mail() {
+
+	// if the submit button is clicked, send the email
+	if ( isset( $_POST['cf-submitted'] ) ) {
+		global $wpdb;
+		// sanitize form values
+		$name    = sanitize_text_field( $_POST["cf-name"] );
+		$email   = sanitize_email( $_POST["cf-email"] );
+		$subject = sanitize_text_field( $_POST["cf-subject"] );
+		$message = esc_textarea( $_POST["cf-message"] );
+
+		// get the blog administrator's email address
+		$to = get_option( 'admin_email' );
+
+		$wpdb->insert( 
+		 'wp_save_contact', 
+		 array( 
+		  'name' => $name, 
+		  'senderEmail' => $email,
+		  'subject' =>$subject,
+		  'message' =>$message,
+		  'adminEmail' =>$to
+		 )  
+		);
+
+
+
+
+
+
+		// include 'include\databaseConnection.php';
+		// $quary="INSERT INTO wp_save_contact (id,name,senderEmail,subject,massage,adminEmail) VALUES ('', '".$name."', '".$email."','".$subject."','".$message."','".$to."')";
+		// echo $quary;
+		// $result=mysql_query($quary,$db_connection);
+		// if($result==false)
+		// {
+		// echo "not executedd";
+		// }
+		// else
+		// {
+		// echo "executed";
+		// }
+		// mysql_close($db_connection);
+
+
+		$headers = "From: $name <$email>" . "\r\n";
+
+		// If email has been process for sending, display a success message
+		if ( wp_mail( $to, $subject, $message, $headers ) ) {
+			echo '<div>';
+			echo '<p>Thanks for contacting me, expect a response soon.</p>';
+			echo '</div>';
+		} else {
+			echo 'An unexpected error occurred';
+		}
+	}
+}
+
+function cf_shortcode() {
+	ob_start();
+	
+	html_form_code();
+	deliver_mail();
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'sitepoint_contact_form', 'cf_shortcode' );
+
+?>
---
+++ widget_plugin/widget_plugin.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+Plugin Name: Custom Widget
+Plugin URI: http://inkthemes.com
+Description: Building a Custom WordPress Widget.
+Author: InkThemes
+Version: 1
+Author URI: http://inkthemes.com
+*/
+
+
+
+
+class site_contact_widget extends WP_Widget {
+
+// constructor
+function __construct() {
+// Give widget name here
+parent::__construct(false, $name = __('site contact Widget', 'wp_widget_plugin') );
+
+}
+
+
+
+function form($instance) {
+
+// Check values
+if( $instance) {
+$title = esc_attr($instance['title']);
+// $textarea = $instance['textarea'];
+} else {
+$title = '';
+// $textarea = '';
+ }
+
+?>
+
+
+
+<p>
+<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+</p>
+<!-- <p>
+<label for="<?php echo $this->get_field_id('textarea'); ?>"><?php _e('Description:', 'wp_widget_plugin'); ?></label>
+<textarea class="widefat" id="<?php echo $this->get_field_id('textarea'); ?>" name="<?php echo $this->get_field_name('textarea'); ?>" rows="7" cols="20" ><?php echo $textarea; ?></textarea>
+</p> -->
+<?php
+}
+
+
+
+
+
+
+//<?php
+
+function update($new_instance, $old_instance) {
+$instance = $old_instance;
+// Fields
+$instance['title'] = strip_tags($new_instance['title']);
+//$instance['textarea'] = strip_tags($new_instance['textarea']);
+return $instance;
+}
+
+
+
+
+//<?php
+// display widget
+function widget($args, $instance) {
+extract( $args );
+
+// these are the widget options
+$title = apply_filters('widget_title', $instance['title']);
+//$textarea = $instance['textarea'];
+echo $before_widget;
+
+
+// Display the widget
+// echo '<div class="widget-text wp_widget_plugin_box" style="width:269px; padding:5px 9px 20px 5px; border: 1px solid rgb(231, 15, 52); background: pink; border-radius: 5px; margin: 10px 0 25px 0;">';
+// echo '<div class="widget-title" style="width: 90%; height:30px; margin-left:3%; ">';
+
+// Check if title is set
+if ( $title ) {
+echo $before_title . $title . $after_title ;
+}
+
+echo do_shortcode('[sitepoint_contact_form]');
+//echo '</div>';
+
+// Check if textarea is set
+//// if( $textarea ) {
+// echo '<p class="wp_widget_plugin_textarea" style="font-size:15px;">'.$textarea.'</p>';
+// }
+// echo '</div>';
+// echo '</div>';
+echo $after_widget;
+} // widget end
+}
+
+
+
+
+
+//<?php
+
+// register widget
+add_action('widgets_init', create_function('', 'return register_widget("site_contact_widget");'));
+
+?>
